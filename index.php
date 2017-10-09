28d327034fd8c06f17b188259bbf4ec4db806ac7
sung <sung1011@gmail.com>
2017-10-10 00:25:51 +0800

    "ig:"

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+
+class abc {
+
+    public $a;
+	public $b;
+
+    public function abc($a)
+    {
+        $this->bbb();
+    }
+
+    /**
+     * Abbb func
+     *
+     * @param string $b 参数
+     *
+     * @return void
+     */
+    public function bbb($b)
+    {
+        $this->a;
+        array_map();
+    }
+
+
+}
+
+
+$o = new abc;
+
